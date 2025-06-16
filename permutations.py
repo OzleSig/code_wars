53dec271fca5f945e235f0486e8488eaf43c7fd9
OzleSig <ozlemsigbeku@protonmail.com>
2025-06-16 12:45:27 +0100

    learnt about sets

--- permutations.py
+++ permutations.py
@@ -1,17 +1,13 @@
-input = 'augggjhjjkw'
+input = 'aaw'
 outdict = dict()
 
 def permutations(input):
     output = []
+    outset = set('')
     if input in outdict:
         return(outdict[input])
     if len(input) == 1:
         return(input)
-    elif len(input) == 2:
-            output.append(input)
-            input = input[1]+input[0]
-            output.append(input)
-            return(output)
     else:
         for i,x in enumerate(input):
                 ansr = []
@@ -19,6 +15,7 @@ def permutations(input):
                     output.append(x+y)
                     ansr.append(y)
                 outdict[input[:i] + input[i+1:]]=ansr 
-    return(output)
+    outset.update(output)
+    return(outset)
     
-permutations(input)
+print(permutations(input))
